fa6e110b85d17bbdfde89827d7b69188ee10c894
gdejulio <garydejulio@yahoo.com>
2015-10-31 11:18:40 -0700

    foo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 'use strict';
 var express = require('express'),
 app = express();
-app.set('views', 'views');
+app.set('views', '/views');
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
 res.render('home', {
